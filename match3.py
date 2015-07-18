4098dbe902c6cba6af61cf5835b600d346ccd428
Ed Paradis <ed.paradis@gmail.com>
2015-07-18 15:01:34 -0700

    add methods to create an empty board and fill boards with empty cells with values

--- match3.py
+++ match3.py
@@ -1,5 +1,6 @@
 import itertools
 import unittest
+import random
 
 def hasMatchOfExactLength(matchLength, row):
     groups = [(k, sum(1 for i in g)) for k,g in itertools.groupby(row)]
@@ -32,6 +33,21 @@ def countMatchN(matchLength, board):
         return min(r,c)
     return countMatchInRows(matchLength, board) + countMatchInColumns(matchLength, board)
 
+def createEmptyBoard( size):
+   return [ [None]*size ] * size 
+
+def fillEmptyCells( board, minVal, maxVal):
+    rng = random.Random()
+    result = []
+    for row in board:
+        newRow = []
+        for cell in row:
+            if cell == None:
+                newRow.append( rng.randint(minVal, maxVal))
+            else:
+                newRow.append( cell)
+        result.append( newRow)
+    return result
 
 class Match3Tests( unittest.TestCase):
     def test_pinningOriginalBehavior(self):
@@ -46,7 +62,34 @@ class Match3Tests( unittest.TestCase):
         self.assertEqual( countMatchThrees(b), 3)
         self.assertEqual( countMatchN(2, a), 1)
         self.assertEqual( countMatchN(1, b), 3)
-
+    def test_createEmptyBoard_fillsBoardWithNone(self):
+        result = createEmptyBoard(3)
+        for row in result:
+            for cell in row:
+                self.assertEqual( cell, None)
+    def test_createEmptyBoard_hasCorrectNumberOfRowsAndColumns(self):
+        result = createEmptyBoard(4)
+        for row in result:
+            self.assertEqual( len(row), 4)
+        self.assertEqual(len(result), 4)
+    def test_fillEmptyCells_leavesNoCellEmpty(self):
+        given = [ [ 1, None],
+                  [ None, 8] ]
+        result = fillEmptyCells(given, 0, 3)
+        for row in result:
+            for cell in row:
+                self.assertNotEqual( cell, None)
+    def test_fillEmptyCells_usesMinToMaxInclusiveValues(self):
+        given = createEmptyBoard(20)
+        result = fillEmptyCells( given, 0, 3)
+        observedValues = []
+        for row in result:
+            for cell in row:
+                if cell not in observedValues:
+                    observedValues.append(cell)
+        observedValues.sort()
+        self.assertListEqual( observedValues, [0, 1, 2, 3] )
+    
 if __name__ == '__main__':
     unittest.main()
 
